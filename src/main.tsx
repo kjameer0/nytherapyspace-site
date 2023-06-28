5b16b6c0dd4784897ae09a60819410e8ce69c5df
Khalid Jameer <kjameer0@gmail.com>
2023-06-28 16:20:55 -0400

    chore: setup basic styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,40 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ErrorPage } from "ErrorPage";
+import { createGlobalStyle } from 'styled-components';
 
-const router = createBrowserRouter([{
-  path: '',
-  element: <App/>,
-  errorElement: <ErrorPage/>,
-  children: []
-}])
+const GlobalStyle = createGlobalStyle<{ $whiteColor?: boolean; $blackBackground?: boolean }>`
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+  body {
+    margin: 0;
+    color: ${(props) => (props.$whiteColor ? 'white' : 'black')};
+    background-color: ${(props) => (props.$blackBackground ? 'black' : 'white')};
+  }
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+`;
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
